75457ef22d09e9d4fe4053ab0755fa852e303a4c
guiijoo <gui.marchetto@hotmail.com>
2024-08-06 20:29:02 -0300

    packages sistema que simula uma clínica para demonstrar a funcionalidade do java através dos pacotes

---
+++ POO com JAVA/pacotes/src/com/mastersoft/clinical/service/ExameService.java
@@ -0,0 +1,11 @@
+package com.mastersoft.clinical.service;
+
+import com.mastersoft.clinical.model.Exame;
+
+public class ExameService {
+    void SalvarExame(Exame exame)
+    {
+        
+    }
+    
+}
