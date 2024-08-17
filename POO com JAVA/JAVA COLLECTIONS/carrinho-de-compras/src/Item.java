0ac8aedb6375d283dc70c61cccc21e22bca6bd07
guiijoo <gui.marchetto@hotmail.com>
2024-08-17 15:02:07 -0300

    Carrinho de Compras

---
+++ POO com JAVA/JAVA COLLECTIONS/carrinho-de-compras/src/Item.java
@@ -0,0 +1,35 @@
+public class Item {
+    
+    //atributos
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    //construtor
+    public Item(String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    //getters
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    //toString
+    public String toString() {
+        return "Item [nome=" + nome + ", preco=" + preco + ", quantidade=" + quantidade + "]";
+    }
+
+    
+    
+}
