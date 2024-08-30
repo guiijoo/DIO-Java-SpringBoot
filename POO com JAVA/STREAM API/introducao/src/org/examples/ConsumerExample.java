d0235de4aae1b4dda93d1ed8ad2afa53bd387147
guiijoo <gui.marchetto@hotmail.com>
2024-08-30 16:38:59 -0300

    STREAM API - Examples Consumer e Supplier

--- POO com JAVA/STREAM API/introducao/src/org/examples/ConsumerExample.java
+++ POO com JAVA/STREAM API/introducao/src/org/examples/ConsumerExample.java
@@ -5,11 +5,11 @@ import java.util.List;
 import java.util.function.Consumer;
 
 public class ConsumerExample {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
     // Criar uma lista de números inteiros
     List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
 
-    // Usar o Consumer com expressão lambda para imprimir números pares
+    // // Usar o Consumer com expressão lambda para imprimir números pares
     Consumer<Integer> imprimirNumeroPar = numero -> {
       if (numero % 2 == 0) {
         System.out.println(numero);
@@ -17,8 +17,6 @@ public class ConsumerExample {
     };
 
     // Usar o Consumer para imprimir números pares no Stream
-    numeros.stream()
-        .filter(n -> n % 2 == 0)
-        .forEach(System.out::println);
+    numeros.stream().forEach(imprimirNumeroPar);
   }
 }
