6ba407cfb5a77e6d9b7bc5e7fc2cafbc3b80cf1b
guiijoo <gui.marchetto@hotmail.com>
2024-10-03 10:07:32 -0300

    API JAVA - Base do projeto

---
+++ SPRING BOOT API/spring-api-dio/src/main/java/me/guiijo/spring_api_dio/SpringApiDioApplication.java
@@ -0,0 +1,13 @@
+package me.guiijo.spring_api_dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiDioApplication.class, args);
+	}
+
+}
---
+++ SPRING BOOT API/spring-api-dio/src/test/java/me/guiijo/spring_api_dio/SpringApiDioApplicationTests.java
@@ -0,0 +1,13 @@
+package me.guiijo.spring_api_dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
