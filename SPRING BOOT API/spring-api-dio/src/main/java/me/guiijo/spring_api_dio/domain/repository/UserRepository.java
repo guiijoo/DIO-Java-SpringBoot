72754dc0d79d8027ec5f03843cea2befcc845814
guiijoo <gui.marchetto@hotmail.com>
2024-10-09 12:46:16 -0300

    API JAVA - Rpository e application-dev

---
+++ SPRING BOOT API/spring-api-dio/src/main/java/me/guiijo/spring_api_dio/domain/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package me.guiijo.spring_api_dio.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import me.guiijo.spring_api_dio.domain.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
